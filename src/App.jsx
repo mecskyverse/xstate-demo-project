5f80245ae71b400c9ba07bc288ea5409d8613d4c
Aakash <vermaaakash1000@gmail.com>
2023-06-11 15:05:26 +0530

    Files updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,46 @@
 import { useState } from 'react'
 import { useMachine } from '@xstate/react'
-import './App.css'
 import { SamagraMachine } from './Machines/DemoMachine'
 
+//THis function is to show a normal api fetch finite states like loading completed or error state
+//we are not doing any api requests we are just trying to demonstrate an typical api calls finite states
+// with help of xstate.
 function App() {
   const [current, send] = useMachine(SamagraMachine)
-  console.log(current.value)
+  const projectArray = ['ABDM',
+    'AI Tools',
+    'Avni',
+    'Bahmni',
+    'Beckn',
+    'CARE',
+    'Cord Network',
+    'cQube',
+    'DevDataPlatform',
+    'DevOps Pipeline',
+    'DIGIT']
+
+  const renderProjects = () => {
+    return projectArray.map((project) => {
+      return (
+        <div className='grid-element'> {project}</div>
+      )
+    })
+  }
   return (
     <>
-      <h1 className='header'>You are in the {current.value} state</h1>
+      <h1 className='header'>You are in the <u>{current.value}</u> state</h1>
       <hr />
       <div className="container">
-        <button>
-          Open Samagra
+        <button onClick={current.value === 'initial' ? () => send('OPENSAMAGRA') : () => send('samagraClose')}>
+
+          {current.value === 'initial' ?
+            'Open Samagra' : 'close Samagra'}
+
+
         </button>
         <div className='frame-section'>
-
+          {current.value === 'completed' ? renderProjects() : null}
+          {/* {renderProjects()} */}
         </div>
       </div>
     </>
--- src/Machines/DemoMachine.js
+++ src/Machines/DemoMachine.js
@@ -1,29 +1,27 @@
-import {createMachine} from 'xstate'
+import {createMachine, send} from 'xstate'
 
 export const SamagraMachine = createMachine({
-    /** @xstate-layout N4IgpgJg5mDOIC5SwIYFsVQE4oHQCMVYwBiAeQAUBRAOQGUBBAWQYHEAlBgbQAYBdRKAAOAe1gBLAC7iRAO0EgAHogCMAVgBMuAGw8AzNo1qVAFgCcADmN6ANCACeiCytxm3bjdpVmA7L7MAvgF2qBjYeKGYOGRCYLIkdAAqDOyJvAJIIKIS0nIKyggmPjy4PhY+mio81c4aJnaOCM64atXV6q1GevpBIehReFgArrKy4rJQJJIoWDCS6QrZUjLymQXaFia4bdUafhp6ambaDU4urW0dPF09vSCyIhBwCpHhi2LLeWuIALQnDr9tNsdiZDioVBU1D5tHdXjgCEQwO8cit8ogTBpTk0XO4PF5-GULLD+uFcHCUDE4sjPqtQAU9BoXFZdjUeNoTDxQVjmrizJ5vH4ympiWF4cNRuMoNTcrSlIgoUCiq0VHp-CYLDwzNycbj+QSLETgiBybgAMYiNBCAA2YEkkGlqO+CEJuCq7Q1ug0x3qAOxrl1+MFFg0QSCQA */
+    /** @xstate-layout N4IgpgJg5mDOIC5SwIYFsVQE4oHQCMVYwBiAeQAUBRAOQGUBBAWQYHEAlBgbQAYBdRKAAOAe1gBLAC7iRAO0EgAHogCMAVgBMuAGw89egOwa1KnioCcAGhABPRBp5aNAFnPmAHAYDMelV+2eAL6B1qgY2HgANiIoEOKyUCSKsJIokmC4KABm6VgAFA56AJQkYZg4uNGx8VC8AkggohLScgrKCM5eWs5qvl5qJpoG2tZ2CA5euAauHu5dKioaGubOwaHo5XhlEWRCYLIkdAAqDOxHdQpNUjLyDe3a5gY6vWbuLtoaXgZWtvaOuC43J4fGZ-EEQiBthUAMYiNBCSJgdIQEipLAwSQXBpXFq3UD3NwAr6aLzmAJedzaFSjRDuFS4GYeFRGZzOHjuVzBCGyEQQOAKKEoS5ia6tO6IAC0I1+CAlalwbkVSuVXjWkI2EQIRDAwuaNzaiGcGhpCE8uDUcy8Kge1qt31VEMFlRicQSutFeKUiC62lw7heize2iNagMJocWmmQO8fQCBjVTsFu327txBoQamc7imixMPA8jgMFnD-yjHhjoICq0dGphcIRSMgqf14tNPEmXmJxg0nmtah+Y3MPFw7M71vMtuZ5gdwSAA */
+   
     id: 'samagra',
-    initial: 'base',
+    initial: 'initial',
     states: {
-        base: {
+        initial: {
             on: {
-                OPENSAMAGRA: 'samagraOpen'
+                OPENSAMAGRA: 'loading'
             }
         },
-        samagraOpen:{
-            on: {
-                START: 'running'
+        loading: {
+            after: {
+                // after 2 second, transition to completed
+                2000: { target: 'completed' }
               }
-        },
-        running: {
-            on: {
-                target:'completed',
-                cond: (_context, event) => event.elapsed >= 3 ? true : false
-            }
           },
-          completed: {
+
+
+        completed: {
             on: {
-                target:'base'
+                samagraClose: 'initial'
             }        
         }
     }
